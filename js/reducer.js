f5bbdfac6fa97f36da9b706b930eb959746a90bc
Taylor <tjaroguetech@gmail.com>
2020-12-28 11:03:11 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -7,6 +7,15 @@ function changeState(state, action){
   }
 }
 
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+function render() {
+  document.body.textContent = state.count
+}
+
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
